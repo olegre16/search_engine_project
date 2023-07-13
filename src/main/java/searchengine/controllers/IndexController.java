8398de071327057af9793db0e4bfe4ea3da9b957
Oleg Gerasimov <olegre@yandex.ru>
2023-07-13 07:48:58 +0300

    add IndexController

--- src/main/java/searchengine/controllers/IndexController.java
+++ src/main/java/searchengine/controllers/IndexController.java
@@ -2,13 +2,11 @@ package searchengine.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import searchengine.dto.statistics.StatisticsResponse;
 import searchengine.services.IndexService;
 
 @RestController
-@RequestMapping("/api")
 public class IndexController {
     private final IndexService indexService;
 
